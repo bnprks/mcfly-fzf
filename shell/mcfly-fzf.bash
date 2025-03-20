78ffce7ec3dde94280d2e4e8ce2a159bfa70ecfd
Michael Cooper <mythmon@gmail.com>
2025-03-20 07:51:22 -0700

    Exit early on non-interactive shells

--- shell/mcfly-fzf.bash
+++ shell/mcfly-fzf.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Exit early on non-interactive shells
+if ! [[ $- =~ .*i.* ]]; then
+  exit 0
+fi
+
 # Ensure stdin is a tty
 # Avoid loading this file more than once
 if [[ -t 0 ]] && [[ "$__MCFLY_FZF_LOADED" != "loaded" ]]; then
@@ -30,37 +35,33 @@ if [[ -t 0 ]] && [[ "$__MCFLY_FZF_LOADED" != "loaded" ]]; then
     export MCFLY_FZF_OPTS=$(command mktemp ${TMPDIR:-/tmp}/mcfly-fzf.json.XXXXXXXX)
   fi
 
-  # If this is an interactive shell, take ownership of ctrl-r.
-  if [[ $- =~ .*i.* ]]; then
+  # Function to perform ctrl-r binding for fzf. Adapted from junegunn/fzf shell/key-bindings.bash
+  __mcfly_fzf_history__() {
+    local output opts script header strict_ordering
+    strict_ordering="$("$MCFLY_FZF_PATH" toggle "$MCFLY_FZF_OPTS" strict-ordering --view)"
+    opts="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS
+        --nth=2.. --delimiter='\t' --no-hscroll --tiebreak=index --read0 --layout reverse
+        --bind=ctrl-r:toggle-sort
+        --bind='ctrl-r:+reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" strict-ordering && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")'
+        --bind='f1:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" sort-order && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")'
+        --bind='f2:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" current-dir-only && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")'
+        --bind='f3:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" exit-code && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")'
+        --ansi
+        --header-lines 1
+        $FZF_CTRL_R_OPTS +m $strict_ordering"
+    output=$(
+      "$MCFLY_FZF_PATH" dump --header -0 --options-json "$MCFLY_FZF_OPTS" |
+        FZF_DEFAULT_OPTS="$opts" fzf --query "$READLINE_LINE"
+    ) || return
+    READLINE_LINE=${output#*$'\t'}
+    "$MCFLY_FZF_PATH" select -- "$READLINE_LINE"
+    if [[ -z "$READLINE_POINT" ]]; then
+      echo "$READLINE_LINE"
+    else
+      READLINE_POINT=0x7fffffff
+    fi
+  }
 
-    # Function to perform ctrl-r binding for fzf. Adapted from junegunn/fzf shell/key-bindings.bash
-    __mcfly_fzf_history__() {
-      local output opts script header strict_ordering
-      strict_ordering="$("$MCFLY_FZF_PATH" toggle "$MCFLY_FZF_OPTS" strict-ordering --view)"
-      opts="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS 
-          --nth=2.. --delimiter='\t' --no-hscroll --tiebreak=index --read0 --layout reverse 
-          --bind=ctrl-r:toggle-sort
-          --bind='ctrl-r:+reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" strict-ordering && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")' 
-          --bind='f1:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" sort-order && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")' 
-          --bind='f2:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" current-dir-only && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")' 
-          --bind='f3:reload(\"$MCFLY_FZF_PATH\" toggle \"$MCFLY_FZF_OPTS\" exit-code && \"$MCFLY_FZF_PATH\" dump --header -0 --options-json \"$MCFLY_FZF_OPTS\")' 
-          --ansi
-          --header-lines 1
-          $FZF_CTRL_R_OPTS +m $strict_ordering"
-      output=$(
-        "$MCFLY_FZF_PATH" dump --header -0 --options-json "$MCFLY_FZF_OPTS" |
-          FZF_DEFAULT_OPTS="$opts" fzf --query "$READLINE_LINE"
-      ) || return
-      READLINE_LINE=${output#*$'\t'}
-      "$MCFLY_FZF_PATH" select -- "$READLINE_LINE"
-      if [[ -z "$READLINE_POINT" ]]; then
-        echo "$READLINE_LINE"
-      else
-        READLINE_POINT=0x7fffffff
-      fi
-    }
-
-    bind -x '"\C-r": __mcfly_fzf_history__'
-  fi
+  bind -x '"\C-r": __mcfly_fzf_history__'
 
 fi
